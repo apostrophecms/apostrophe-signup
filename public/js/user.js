249fe1e5a3153447f299cd7b2d67250b56b63344
Thomas Boutell <tom@punkave.com>
2019-02-27 11:05:46 -0500

    password field should be required, also fixed an issue where all fields appeared as having errors if you tried to sign up with a duplicate username or email address. This required some compromises to achieve bc with existing markup and css. Added busy spinner where appropriate as well.

--- public/js/user.js
+++ public/js/user.js
@@ -3,22 +3,21 @@ apos.define('apostrophe-signup', {
     self.enable = function(options) {
       $(function() {
         var user = apos.schemas.newInstance(options.schema);
-        // apos.ui.globalBusy(true);
         var $signup = $('[data-apos-signup]');
-        console.log('populating');
+        apos.ui.globalBusy(true);
         apos.schemas.populate($signup, options.schema, user, function(err) {
-          console.log('in callback');
-          // apos.ui.globalBusy(false);
-          console.log('not busy anymore');
+          apos.ui.globalBusy(false);
           if (err) {
-            apos.notify('An error occurred populating the signup form.');
+            // apos.notify requires a user, use alert
+            alert('An error occurred populating the signup form.');
             return;
           }
         });
 
         $('body').on('click', '[data-apos-signup-submit]', function(e) {
           apos.ui.globalBusy(true);
-
+          var $email = apos.schemas.findFieldset($signup, 'email');
+          var $username = apos.schemas.findFieldset($signup, 'username');
           async.series([
             convert,
             submit 
@@ -29,6 +28,11 @@ apos.define('apostrophe-signup', {
             return apos.schemas.convert($signup, options.schema, user, callback);
           }
           function submit(callback) {
+            // If no schema error by this point, clear error class on these
+            // two fields in case we put it there for an earlier conflict.
+            // We can put it back after submit if necessary
+            $email.removeClass('apos-error');
+            $username.removeClass('apos-error');
             return $.jsonCall(options.url, user, function(result) {
               return callback((result.status === 'ok') ? null : result.status); 
             }, callback);
@@ -36,10 +40,23 @@ apos.define('apostrophe-signup', {
           function displayResult(err) {
             apos.ui.globalBusy(false);
             if (err) {
-              $signup.addClass('apos-error');
+              if (Array.isArray(err)) {
+                // convert already displayed it
+              } else {
+                // treat it as an error on email and username,
+                // the most likely suspects
+                $email.addClass('apos-error');
+                $username.addClass('apos-error');
+                // for bc find this existing class and just show it,
+                // bypassing the problem that legacy CSS won't
+                // find it in a parent with apos-error anymore
+                // (doing that made all the other field errors
+                // show up, which is wrong)
+                $signup.find('.apos-signup-error').show();
+              }
             } else {
-              $signup.removeClass('apos-error');
               $signup.addClass('apos-success');
+              $signup.find('.apos-signup-error').hide();
             }
           }
           return false;
